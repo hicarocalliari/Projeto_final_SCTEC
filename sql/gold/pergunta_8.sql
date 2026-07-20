d110c6a04d609d6057b10b430b5b416d6748f071
Hicaro Calliari Bottenberg <58341270+hicarocalliari@users.noreply.github.com>
2026-07-19 22:57:20 -0300

    Novas imagens

--- sql/gold/pergunta_8.sql
+++ sql/gold/pergunta_8.sql
@@ -1,9 +1,16 @@
 SELECT
     st.meio_transporte,
-    AVG(sv.valor_total) AS custo_medio
-FROM silver_trecho st
-INNER JOIN silver_viagem sv
-    ON st.id_viagem = sv.id_viagem
-WHERE st.meio_transporte IS NOT NULL
-GROUP BY st.meio_transporte
-ORDER BY custo_medio DESC;
+    AVG(v.valor_total) AS custo_medio
+FROM (
+    SELECT DISTINCT
+        id_viagem,
+        meio_transporte
+    FROM silver_trecho
+    WHERE meio_transporte IS NOT NULL
+) st
+INNER JOIN silver_viagem v
+    ON st.id_viagem = v.id_viagem
+GROUP BY
+    st.meio_transporte
+ORDER BY
+    custo_medio DESC;
--- sql/gold/pergunta_9.sql
+++ sql/gold/pergunta_9.sql
@@ -1,13 +1,17 @@
 SELECT
-    CONCAT(st.destino_cidade, '/', st.destino_uf) AS destino,
+    st.destino,
     SUM(sv.valor_total) AS custo_total
-FROM silver_trecho st
+FROM (
+    SELECT DISTINCT
+        id_viagem,
+        CONCAT(destino_cidade, '/', destino_uf) AS destino
+    FROM silver_trecho
+    WHERE destino_cidade IS NOT NULL
+) st
 INNER JOIN silver_viagem sv
     ON st.id_viagem = sv.id_viagem
-WHERE 
-    st.destino_cidade IS NOT NULL
 GROUP BY
-    st.destino_cidade,
-    st.destino_uf
-ORDER BY custo_total DESC
+    st.destino
+ORDER BY
+    custo_total DESC
 LIMIT 10;
